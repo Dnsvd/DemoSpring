de6a9e34094b388537b86b4e7e7eaf1a81b4af89
Denis Denisov <dnsvd23@gmail.com>
2025-05-08 09:46:25 +0300

    Init

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    //1 вариант SQL запроса
+    @Query(value = "select * from user where lower(email) = lower(:email)", nativeQuery = true)
+    Optional<User> findByUserEmail(@Param(value = "email") String email);
+    // 2 вариант SQL запроса
+//    @Query(value = "select u from User u where u.email = :email")
+//    User findByEmail(@Param(value = "email") String email);
+    //3 вариант
+//    User findByEmailAndAgeAfterAndBirthday(String email, int age, LocalDate birthday);
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import com.example.demo.repository.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User create(User user) {
+        Optional<User> optionalUser = userRepository.findByUserEmail(user.getEmail());
+        if (optionalUser.isPresent()) {
+            throw  new IllegalStateException("User already exists");
+        }
+        user.setAge(Period.between(user.getBirthday(), LocalDate.now()).getYears());
+        return userRepository.save(user);
+    }
+
+}
